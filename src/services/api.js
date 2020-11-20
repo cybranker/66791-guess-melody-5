263fb61915d92faf431240d8eedf926dd1b4653d
cybranker <cybranker@yandex.ru>
2020-11-20 13:47:00 +0200

    Добавляет загрузку списка вопросов

--- src/services/api.js
+++ src/services/api.js
@@ -3,11 +3,7 @@ import axios from "axios";
 const BACKEND_URL = `https://5.react.pages.academy/guess-melody`;
 const REQUEST_TIMEOUT = 5000;
 
-const HttpCode = {
-  UNAUTHORIZED: 401
-};
-
-export const createAPI = (onUnauthorized) => {
+export const createAPI = () => {
   const api = axios.create({
     baseURL: BACKEND_URL,
     timeout: REQUEST_TIMEOUT,
@@ -17,14 +13,6 @@ export const createAPI = (onUnauthorized) => {
   const onSuccess = (response) => response;
 
   const onFail = (err) => {
-    const {response} = err;
-
-    if (response.status === HttpCode.UNAUTHORIZED) {
-      onUnauthorized();
-
-      throw err;
-    }
-
     throw err;
   };
 
